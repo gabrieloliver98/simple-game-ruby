ce7d6ad5f3cdeec39c18e44ff247bd78821d374b
gabrieloliver98 <gabrieloliver8991@gmail.com>
2017-10-14 18:44:47 -0300

    Adicionando 'initialize' na 'RandRoom'

--- scenario.rb
+++ scenario.rb
@@ -3,7 +3,12 @@ class Scene
 end
 
 class RandRoom < Scene
-
+  attr_accessor :portal_key
+  def initialize()
+    enemies = rand(0..10)
+    alive = Fight(enemies)
+    return alive
+  end
 end
 
 class InitialRoom < Scene
