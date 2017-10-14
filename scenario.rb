f777d7d914c2ae4d682b5b4ce3e7a24897477f86
gabrieloliver98 <gabrieloliver8991@gmail.com>
2017-10-14 19:04:17 -0300

    adicionando 'enter()' a 'RandRoom'

--- scenario.rb
+++ scenario.rb
@@ -8,10 +8,16 @@ class RandRoom < Scene
   attr_accessor :portal_key
   def initialize()
     @enemies = rand(0..10)
-  
   end
 
-
+  def enter()
+    puts "You entered in a new room"
+    puts "You stil not has the portal key"
+    puts "Now you will fight with #{@enemies} enemies"
+    fight = Fight.new()
+    alive = fight(@enemies)
+    return alive
+  end
 end
 
 class InitialRoom < Scene
