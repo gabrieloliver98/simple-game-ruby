2fe84eec0c406bc315a8ec1d622e7c99eee4fda2
gabrieloliver98 <gabrieloliver8991@gmail.com>
2017-10-14 18:31:15 -0300

    Adicionando 'initialize' e 'play' na 'Engine'

--- main.rb
+++ main.rb
@@ -2,7 +2,22 @@ require 'scenario.rb'
 require 'fighting.rb'
 
 class Engine
+  def initialize()
+    @hero = Player.new()
+  end
 
+  def play()
+    InitialRoom.enter()
+    where_is_portalkey = rand(1..10)
+    count = 1
+    while self.hero.portal_key == false
+        if where_is_portalkey == count
+          current_room.portal_key == true
+        end
+        count += 1
+    end
+    FinalRoom.enter()
+  end
 end
 
 a_game = Engine.new()
