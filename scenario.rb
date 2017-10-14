3e4b08bb4e3f677af0867636cdcce6c35032b4bf
gabrieloliver98 <gabrieloliver8991@gmail.com>
2017-10-14 19:16:36 -0300

    Inserindo enter() em 'InitialRoom'

--- scenario.rb
+++ scenario.rb
@@ -21,7 +21,15 @@ class RandRoom < Scene
 end
 
 class InitialRoom < Scene
-
+  def self.enter()
+    puts "You woke up in a stange place."
+    puts "You see a couple of aliens."
+    puts "They explain to you that they have captured you."
+    puts "And they want you to explain how to make your techology of portal key"
+    puts "You can't do that, so they tell if you not cooperate, they don't will give your portal key back."
+    puts "What will you do?"
+    puts ">"
+  end
 end
 
 class FinalRoom < Scene
