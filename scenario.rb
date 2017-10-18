cbf13fd1e2e48c45783766d2bf1abf23f5bc2a8e
gabrieloliver98 <gabrieloliver8991@gmail.com>
2017-10-18 00:34:36 -0200

    Colocando 'result()' em 'Scene'

--- scenario.rb
+++ scenario.rb
@@ -4,6 +4,25 @@ class Scene
   def enter()
     puts "If this message is being showed, this room is with a problem :)"
   end
+
+  def result()
+    puts "What will you do?"
+    print ">"
+    command = $stdin.gets.chomp
+    command.downcase!
+    if command == "attack"
+      puts "The aliens run way, but you still without your portal key"
+    elsif command == "cry"
+      puts "The aliens will kill you"
+      puts "You are dying"
+      puts "When...suddenly..."
+      puts "You DIE"
+      exit(0)
+    else
+      puts "Learn how to write :P"
+      exit(0)
+    end
+  end
 end
 
 class RandRoom < Scene
@@ -16,42 +35,26 @@ class RandRoom < Scene
     puts "You entered in a new room"
     puts "But you don't have the portal key"
     puts "Now you will fight with one enemie"
-    fight = Fight.new()
-    alive = fight.fight(hero, @enemie)
+    result()
+    current_fight = Fight.new()
+    alive = current_fight.fight(hero, @enemie)
     return alive
   end
 end
 
 class InitialRoom < Scene
-  def self.enter()
+  def initialize()
     puts "You woke up in a stange place."
     puts "You see some aliens."
     puts "They explain to you that they have captured you."
     puts "And they want you to explain how to make your techology of portal key"
     puts "You can't do that, so they tell if you not cooperate, they don't will give your portal key back."
-    puts "What will you do?"
-    print ">"
-    command = $stdin.gets.chomp
-    result(command)
-  end
-
-  def self.result(command)
-    command.downcase!
-    if command == "attack"
-      puts "The aliens run way, but you still without your portal key"
-    elsif command == "cry"
-      puts "The aliens will kill you"
-      puts "You are dying"
-      puts "When...suddenly..."
-      puts "You DIE"
-    else
-      puts "Learn how to write :P"
-    end
+    result()
   end
 end
 
 class FinalRoom < Scene
-  def self.enter()
+  def initialize()
     puts "You achived the portal key"
     puts "Now, you can get back home"
     puts "Thanks for playing this game"
